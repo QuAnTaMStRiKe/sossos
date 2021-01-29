1c20dde8802d6c8b269e1b3d83ccb5cfa5ee36f8
ANSH <quantamstrike@gmail.com>
2021-01-30 01:21:05 +0530

    COLOUR CHANGED

--- app/src/main/java/com/example/sos/OnClickUserProfile.kt
+++ app/src/main/java/com/example/sos/OnClickUserProfile.kt
@@ -1,11 +1,17 @@
 package com.example.sos
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_on_click_user_profile.*
 
 class OnClickUserProfile : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_on_click_user_profile)
+        backBtn1.setOnClickListener {
+            val i = Intent(this, MainActivity::class.java)
+            startActivity(i)
+        }
     }
 }
