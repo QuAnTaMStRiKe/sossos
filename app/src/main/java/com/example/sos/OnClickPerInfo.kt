0260cd9470ae80422bfa9316545f0d726407c9f5
ANSH <quantamstrike@gmail.com>
2021-01-30 02:00:13 +0530

    COLOUR CHANGED 4

---
+++ app/src/main/java/com/example/sos/OnClickPerInfo.kt
@@ -0,0 +1,11 @@
+package com.example.sos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OnClickPerInfo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_on_click_per_info)
+    }
+}
